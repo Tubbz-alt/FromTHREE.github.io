f0b8a364ed9bee29d49ab143d480218d0779981d
blueyy <464176845@qq.com>
2018-03-12 23:06:53 +0800

    normal face

--- docinstance/geo2050/basicScene.js
+++ docinstance/geo2050/basicScene.js
@@ -1,4 +1,7 @@
 const radius = 50
+const CITY_RADIUS = 1
+const BLING_CITY_HEIGHT = 20
+const BLING_CITY_WIDTH = 1
 function createBasicScene () {
 	const WIDTH = window.innerWidth
 	const HEIGHT = window.innerHeight
@@ -18,6 +21,17 @@ function createBasicScene () {
   trackball.zoomSpeed = 1.0
   trackball.panSpeed = 1.0
 
+  ambiLight = new THREE.AmbientLight(0x141414)
+  ambiLight.position.set(0, 120, 120)
+  scene.add(ambiLight)
+
+  directionalLight = new THREE.DirectionalLight()
+  directionalLight.position.set(0, 200, 200)
+  scene.add(directionalLight)
+
+  var axes = new THREE.AxesHelper(50)
+  scene.add(axes)
+
   clock = new THREE.Clock()
 }
 
@@ -36,55 +50,127 @@ function createBasicMesh (type, options, pos) {
 		color: 0xcd664e
 	})
 	let mesh = new THREE.Mesh(geometry, material)
-	mesh.position.x = pos.x || 0
-	mesh.position.y = pos.y || 0
-	mesh.position.z = pos.z || 0
+	setPosition(mesh, pos)
 	return mesh
-}
-
-function createTextureCircle (r, segments, lnglat, url) {
-	let geometry = new THREE.CircleGeometry(r, segments)
-	let texture = new THREE.TextureLoader().load(url)
-	let material = new THREE.MeshPhongMaterial()
-	material.map = texture
-	console.log(material)
-	let mesh = new THREE.Mesh(geometry, material)
-	let position = convertFromLnglatToVec3(lnglat)
-	mesh.position.x = position.x
-	mesh.position.y = position.y
-	mesh.position.z = position.z
-	return mesh
-}
-
-function convertFromLnglatToVec3 (lnglat) {
-	const lng = lnglat[0]
-	const lat = lnglat[1]
-	const phi = (180 - lng) * (Math.PI / 180)
-	const theta = (90 + lat) * (Math.PI / 180)
-	const x = radius * Math.sin(theta) * Math.cos(phi)
-	const y = radius * Math.sin(theta) * Math.sin(phi)
-	const z = radius * Math.cos(theta)
-	return {x, y, z}
 }
 
 function createLinesMesh (lines) {
+	let material = new THREE.MeshBasicMaterial({
+		color: 0x03d98e
+	})
 	lines.forEach((line, i) => {
-		const length = line.length
-		let vertices = new Float32Array(length * 3)
-		let bufferGeom = new THREE.BufferGeometry()
+		let geometry = new THREE.Geometry()
+		let vertices = []
 		line.forEach((lnglat, i) => {
 			const pos = convertFromLnglatToVec3(lnglat)
-			vertices[i * 3] = pos.x
-			vertices[i * 3 + 1] = pos.y
-			vertices[i * 3 + 2] = pos.z
+			vertices.push(new THREE.Vector3(pos.x, pos.y, pos.z))
 		})
-		bufferGeom.addAttribute('position', new THREE.BufferAttribute(vertices, 3))
-		let material = new THREE.MeshBasicMaterial({
-			// color: 0x02e095
-			color: 0xffff00
-		})
-		let mesh = new THREE.Mesh(bufferGeom, material)
+		let curve = new THREE.CatmullRomCurve3(vertices)
+		geometry.vertices = curve.getPoints(1000)
+		let mesh = new THREE.Line(geometry, material)
 		scene.add(mesh)
+	})
+}
+
+function createBlingCity (lnglat) {
+	const position = convertFromLnglatToVec3(lnglat)
+	let hexagonLine = new THREE.CircleGeometry(CITY_RADIUS, 6)
+	let hexagonPlane = new THREE.CircleGeometry(CITY_RADIUS - 0.3, 6)
+	let vertices = hexagonLine.vertices
+	vertices.shift() // 第一个节点是中心点
+	let circleLineGeom = new THREE.Geometry()
+	circleLineGeom.vertices = vertices
+	let material = new THREE.MeshBasicMaterial({
+		color: 0xffffff,
+		side: THREE.DoubleSide
+	})
+	let circleLine = new THREE.LineLoop(circleLineGeom, material)
+	let circlePlane = new THREE.Mesh(hexagonPlane, material)
+	setPosition(circleLine, position)
+	setPosition(circlePlane, position)
+	circlePlane.lookAt(new THREE.Vector3(0, 0, 0))
+	circleLine.lookAt(new THREE.Vector3(0, 0, 0))
+	scene.add(circleLine)
+	scene.add(circlePlane)
+}
+
+function createTextureCity (lnglat, url) {
+	let geometry = new THREE.PlaneGeometry(BLING_CITY_WIDTH, BLING_CITY_HEIGHT)
+	// geometry.faceVertexUvs[0][0] = [
+	// 	new THREE.Vector2(1, 0),
+	// 	new THREE.Vector2(1, 1),
+	// 	new THREE.Vector2(0, 0)
+	// ]
+	// geometry.faceVertexUvs[0][1] = [
+	// 	new THREE.Vector2(1, 1),
+	// 	new THREE.Vector2(0, 1),
+	// 	new THREE.Vector2(0, 0)
+	// ]
+
+	let material
+	let position = convertFromLnglatToVec3(lnglat)
+	const vecPosition = new THREE.Vector3(position.x, position.y, position.z)
+
+	let geom = new THREE.Geometry()
+	geom.vertices = [
+		new THREE.Vector3(0, 0, 0),
+		vecPosition
+	]
+	let mat = new THREE.MeshBasicMaterial({
+		color: 0x00ffff
+	})
+	let m = new THREE.Line(geom, mat)
+	scene.add(m)
+
+	let texture = new THREE.TextureLoader().load(url, (texture) => {
+		material = new THREE.MeshBasicMaterial({
+			map: texture,
+			side: THREE.DoubleSide
+		})
+		let mesh1 = new THREE.Mesh(geometry, material)
+		setPosition(mesh1, position)
+
+		mesh1.rotateX(Math.PI / 2)
+		mesh1.translateZ(-1 * BLING_CITY_HEIGHT / 2)
+		const rotation1 = computeRotation(mesh1.position, vecPosition)
+		mesh1.rotateX(rotation1.x)
+		mesh1.rotateY(rotation1.y)
+		mesh1.rotateZ(rotation1.z)
+		mesh1.position.normalize()
+		mesh1.translateY(-1 * radius - BLING_CITY_HEIGHT / 2)
+
+		// let axis = mesh1.position.clone()
+		// let mesh2 = mesh1.clone()
+		// mesh2.rotateOnAxis(axis.normalize(), Math.PI / 2)
+		// mesh2.rotateOnAxis(axis.normalize(), Math.PI / -2)
+
+
+		let mesh2 = new THREE.Mesh(geometry, material)
+
+		mesh2.rotateX(Math.PI / 2)
+		mesh2.translateZ(-1 * BLING_CITY_HEIGHT / 2)
+		mesh2.rotateZ(Math.PI / 2)
+
+		// setPosition(mesh2, position)
+		
+
+		// const rotation2 = computeRotation(mesh2.position, vecPosition)
+		// mesh2.rotateX(rotation2.x)
+		// mesh2.rotateY(rotation2.y)
+		// mesh2.rotateZ(rotation2.z)
+		// mesh2.position.normalize()
+		// mesh2.rotateY(Math.PI / 2)
+		// mesh2.translateY(-1 * radius - BLING_CITY_HEIGHT / 2)
+		// mesh2.translateX(mesh2.position.x - mesh1.position.x)
+		// mesh2.translateZ(mesh2.position.z - mesh1.position.z)
+		// mesh2.translateY(mesh2.position.y - mesh1.position.y)
+
+
+
+
+
+		scene.add(mesh1)
+		scene.add(mesh2)
 	})
 }
 
@@ -93,4 +179,38 @@ function animate () {
   var delta = clock.getDelta()
   trackball.update(delta)
 	renderer.render(scene, camera)
+}
+
+// 功能函数
+function convertFromLnglatToVec3 (lnglat) {
+	const lng = lnglat[0]
+	const lat = lnglat[1]
+	const phi = (180 - lng) * (Math.PI / 180)
+	const theta = (90 + lat) * (Math.PI / 180)
+	const x = radius * Math.sin(theta) * Math.cos(phi)
+	const y = radius * Math.sin(theta) * Math.sin(phi)
+	const z = radius * Math.cos(theta)
+
+	return {x, y, z}
+}
+
+function setPosition (mesh, position) {
+	// if (!mesh.isObject) {
+	// 	console.error('mesh is not Object3D!')
+	// 	return
+	// }
+	if (position.x === undefined || position.y === undefined || position.z === undefined) {
+		console.error('position is unvalid!')
+		return
+	}
+	mesh.position.x = position.x
+	mesh.position.y = position.y
+	mesh.position.z = position.z
+}
+
+function computeRotation (source, target) {
+	const x = source.angleTo(new THREE.Vector3(0, target.y, target.z))      
+	const y = source.angleTo(new THREE.Vector3(target.x, 0, target.z))
+	const z = source.angleTo(new THREE.Vector3(target.x, target.y, 0))
+	return {x, y, z}
 }
